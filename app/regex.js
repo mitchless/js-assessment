e3cfb49aed32902d0daec00fd5da6b96ed463d5e
Mitch Oliver <mitch.oliver@gmail.com>
2012-11-19 23:55:22 -0500

    Finish tests

--- app/regex.js
+++ app/regex.js
@@ -1,28 +1,42 @@
+// vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
     containsNumber : function(str) {
-
+      var re = /\d+/;
+      return re.test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+      var re = /(\D)\1/;
+      return re.test(str);
     },
 
     endsWithVowel : function(str) {
-
+      var re = /[AaEeIiOoUu]\b/;
+      return re.test(str);
     },
 
     captureThreeNumbers : function(str) {
+      var re = /\d{3}/,
+        result = re.exec(str);
 
+      if (result) {
+        return result[0];
+      } else {
+        return false;
+      }
     },
 
     matchesPattern : function(str) {
-
+      var re = /\b\d{3}-\d{3}-\d{4}\b/;
+      return re.test(str);
     },
-    isUSD : function(str) {
 
+    isUSD : function(str) {
+      var re = /^\$(?:\d{1,3})(?:,\d{3})*(?:\.\d{2}$|$)/;
+      return re.test(str);
     }
   };
 });
